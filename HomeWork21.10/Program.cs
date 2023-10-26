f08380ace44a56a4e65040652b1b9c15542c3124
yarrobong <oneboardsholi@gmail.com>
2023-10-26 10:22:47 +0500

    Добавьте файлы проекта.

---
+++ HomeWork21.10/Program.cs
@@ -0,0 +1,15 @@
+﻿string s = "Привет";
+Console.WriteLine(s[5]);
+Console.WriteLine(s[s.Length - 1]);
+Console.WriteLine();
+
+for (int k=0; k < s.Length; k++)
+{
+    Console.WriteLine(s[k]);
+}
+Console.WriteLine();
+
+foreach (char item in s)
+{
+    Console.WriteLine(item);
+}
